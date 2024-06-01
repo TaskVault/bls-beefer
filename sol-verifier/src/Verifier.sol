36bc4ac12e3c1b701acd3597724239e9d5b87972
Marios Isaakidis <prometheas@autistici.org>
2024-06-01 13:49:19 +0200

    Remap @summa-tx/memview.sol lib; Downgrade pragma solidity version

--- sol-verifier/src/Verifier.sol
+++ sol-verifier/src/Verifier.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.6.0;
 
 import { B12_381Lib } from "b12-sol/B12.sol";
 
 contract Verifier {
-    function verify(B12.PairingArg[] memory argVec) public pure returns (bool) {
+    function verify(B12_381Lib.PairingArg[] memory argVec) public pure returns (bool) {
         return pairing(argVec);
     }
 }
--- sol-verifier/test/Verifier.t.sol
+++ sol-verifier/test/Verifier.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.6.0;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Verifier} from "../src/Verifier.sol";
